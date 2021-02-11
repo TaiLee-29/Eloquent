e7d64eb51d3e995093b9dae0c1804ec95af3966e
TaiLee-29 <dreih796@gmail.com>
2021-02-11 19:02:34 +0200

    1 part done

--- public/index.php
+++ public/index.php
@@ -31,6 +31,7 @@ require_once '../config/eloquent.php';
 
 //$category5 = \Hillel\model\category::find(5);
 //$category5->title = 'Drama';
+//$category5->slug ='Not recommended for people with mental issues';
 //$category5->save();
 
 
@@ -38,5 +39,64 @@ require_once '../config/eloquent.php';
 //$category->delete();
 //END OF 1 PART OF HOMEWORK
 
-
+/*$post = new Hillel\model\post();
+$post->title = 'Atlantida';
+$post->slug = 'Answer on your question you can find in king eyes';
+$post->body = 'Introduction and eleven charpets of story';
+$post->category_id = '2';
+$post->save();
+$post2 = new Hillel\model\post();
+$post2->title = 'Atlantida2';
+$post2->slug = 'New queen-- KIDA';
+$post2->body = 'Introduction and eighteen charpets of story';
+$post2->category_id = '2';
+$post2->save();
+$post3 = new Hillel\model\post();
+$post3->title = 'Atlantida3';
+$post3->slug = 'Nautilus and fallen dinasty';
+$post3->body = 'Introduction and ten charpets of story';
+$post3->category_id = '2';
+$post3->save();
+$post4 = new Hillel\model\post();
+$post4->title = 'Midas';
+$post4->slug = 'Did gold is the everything that you need?';
+$post4->body = 'Short story about thief who became a legend ';
+$post4->category_id = '5';
+$post4->save();
+$post5 = new Hillel\model\post();
+$post5->title = 'Simple geometry  ';
+$post5->slug = 'Your brain - your life';
+$post5->body = '23 charpets and 12 pages with answers';
+$post5->category_id = '3';
+$post5->save();
+$post6 = new Hillel\model\post();
+$post6->title = 'Healthy body - healthy soul';
+$post6->slug = 'If you can control your body - you can do everything';
+$post6->body = '457 pages with popular description and illustrations ';
+$post6->category_id = '5';
+$post6->save();
+$post7 = new Hillel\model\post();
+$post7->title = 'Lie to me';
+$post7->slug = 'Simple ways to understand when someone lie to you';
+$post7->body = '700 pages of useless information';
+$post7->category_id = '5';
+$post7->save();
+$post8 = new Hillel\model\post();
+$post8->title = 'Algebra in everyday life';
+$post8->slug = 'Burn in hell';
+$post8->body = '973 Hellish cauldron';
+$post8->category_id = '3';
+$post8->save();
+$post9 = new Hillel\model\post();
+$post9->title = 'PHP manual';
+$post9->slug = 'Very aggressive homeworks ';
+$post9->body = '300 hundred bucks and dungeon muster ';
+$post9->category_id = '2';
+$post9->save();
+$post10 = new Hillel\model\post();
+$post10->title = 'Acid for ex';
+$post10->slug = 'Let him die with his whores in one day';
+$post10->body = 'Most important manual for teenagers ';
+$post10->category_id = '2';
+$post10->save();*/
 
